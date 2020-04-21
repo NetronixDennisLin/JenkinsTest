9e742d64746501eaea97b33be9eda14a817961a3
Dennis <dennis.lin.iisi@gmail.com>
2020-04-21 13:18:47 +0800

    add selenium test

--- Mvc5UnitTesting/UnitTest1.cs
+++ Mvc5UnitTesting/UnitTest1.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
 
 namespace Mvc5UnitTesting
 {
@@ -9,6 +12,18 @@ namespace Mvc5UnitTesting
         [TestMethod]
         public void TestMethod1()
         {
+        }
+
+        [TestMethod]
+        public void test01()
+        {
+            ChromeDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.google.com/");
+            driver.Manage().Window.Size = new System.Drawing.Size(1054, 664);
+            driver.FindElement(By.Name("q")).Click();
+            driver.FindElement(By.Name("q")).SendKeys("selenium");
+            driver.FindElement(By.Name("q")).SendKeys(Keys.Enter);
+            driver.FindElement(By.LinkText("維基百科")).Click();
         }
     }
 }
